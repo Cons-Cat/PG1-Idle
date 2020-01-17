99a3b3a7f6477b4ff76563f50dcb4ddd4c3f19bb
Dmac15 <59742911+Dmac15@users.noreply.github.com>
2020-01-17 11:07:36 -0500

    Loop Codes
    - I added a simple loop that reads letter keys to change the multiplier to the school
    -Have to work on reading number keys

--- Source/Program.cs
+++ Source/Program.cs
@@ -1,15 +1,80 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Source
+namespace CheckLoop
 {
-	class Program
-	{
-		static void Main( string[] args )
-		{
-		}
-	}
-}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 0;
+            int val;
+            Console.WriteLine(start);
+            Console.WriteLine("Press the spacebar to collect");
+            Console.WriteLine("Press X to quit");
+            while (true)
+            {
+                ConsoleKeyInfo user = Console.ReadKey();
+                Console.Clear();
+
+                if (user.Key == ConsoleKey.Spacebar)
+                {
+                    val = start += 1;
+                    Console.Clear();
+                    Console.WriteLine(val);
+                }
+                else if (user.Key == ConsoleKey.W)
+                {
+                    val = start += 2;
+                    Console.Clear();
+                    Console.WriteLine(val);
+                }
+                else if(user.Key == ConsoleKey.E)
+                {
+                    val = start += 5;
+                    Console.Clear();
+                    Console.WriteLine(val);
+                }
+                else if(user.Key == ConsoleKey.R)
+                {
+                    val = start += 10;
+                    Console.Clear();
+                    Console.WriteLine(val);
+                }
+                else if (user.Key == ConsoleKey.A)
+                {
+                    val = start += 12;
+                    Console.Clear();
+                    Console.WriteLine(val);
+                }
+                else if (user.Key == ConsoleKey.S)
+                {
+                    val = start += 15;
+                    Console.Clear();
+                    Console.WriteLine(val);
+                }
+                else if (user.Key == ConsoleKey.D)
+                {
+                    val = start += 20;
+                    Console.Clear();
+                    Console.WriteLine(val);
+                }
+                else if (user.Key == ConsoleKey.X)
+                {
+                    val = start;
+                    Console.Clear();
+                    Console.WriteLine("Thanks for playing! Your final score is: " + val);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("You broke the game! Happy!?");
+                    break;
+                }
+            }
+
+         } 
+
+            
+        }
+    }
+
