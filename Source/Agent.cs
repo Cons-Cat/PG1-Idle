24f84b61a944b30b0804f78f943f965d307fea1a
William Gooch <wgooch2000@gmail.com>
2020-01-18 22:43:46 -0500

    Created Rudimentary Threads
    - Removed deprecated code.
    - Renamed Mathcore.cs to Agent.cs to better express its current behavior.
    - Abstracted behavior in the while (true) {} loop.
    - Made that abstracted behavior execute in two asynchronous threads.

---
+++ Source/Agent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Source
+{
+    // Agent object behavior
+    public class Agent {
+        int index;
+        public double pointsRate { get; set; }      // Arbitrary rate that this agent index generates points.
+        public double priceFactor { get; set; }     // Arbitrary factor for price of another agent.
+        public uint agentCount { get; set; }         // Arbitrary count of this agent. Used as a multiplier for pointsRate and priceFactor.
+        
+        public Agent(int argIndex, double argPointsRate, double argPriceFactor)
+        {
+            index = argIndex;
+            pointsRate = argPointsRate;
+            priceFactor = argPriceFactor;
+        }
+    }
+}
--- Source/Program.cs
+++ Source/Program.cs
@@ -1,51 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Source
 {
     class Program
     {
-        static void Main(string[] args)
+        // Instantiation
+        static Agent[] agentObjsArr = new Agent[10]; // 10 agents
+        static RenderWindow renderObj = new RenderWindow(9, 3); // 10 rows, 4 columns
+
+        static class GameOperator
         {
-            // Initialize agents
-            Agent[] agentObjsArr = new Agent[10];
-            agentObjsArr[0] = new Agent(0,1.1,10);
-            agentObjsArr[1] = new Agent(1,1.2,50);
-            agentObjsArr[2] = new Agent(2,1.4,150);
-            agentObjsArr[3] = new Agent(3,1.75,200);
-            agentObjsArr[4] = new Agent(4,1,300);
-            agentObjsArr[5] = new Agent(5,1.5,400);
-            agentObjsArr[6] = new Agent(6,2,550);
-            agentObjsArr[7] = new Agent(7,4,700);
-            agentObjsArr[8] = new Agent(8,7,1000);
-            agentObjsArr[9] = new Agent(9,10,2000);
+            static public double gamePoints { get; set; }
+        }
 
-            // Render console window
-            RenderWindow renderObj = new RenderWindow(9, 3);
+        static void UpdateConsole()
+        {
+            // Update the renderer's points.
+            RenderWindow.displayPoints = GameOperator.gamePoints;
             renderObj.RenderLoop();
+        }
 
+        static void GameLoop()
+        {
             while (true)
             {
                 // Loop through all agents
-                for (int i = 0; i < 10; i++) {
-                    GameOperator.gamePoints += agentObjsArr[i].pointsRate * agentObjsArr[i].agentCount; // Increment points by the agents.
+                for (int i = 0; i < 10; i++)
+                {
+                    GameOperator.gamePoints += agentObjsArr[i].pointsRate * agentObjsArr[i].agentCount; // Point incrementing algorithm.
                     RenderWindow.agentCount[i] = agentObjsArr[i].agentCount;
-                    RenderWindow.agentPrice[i] = (uint)(agentObjsArr[i].priceFactor * (agentObjsArr[i].agentCount+1));
+                    RenderWindow.agentPrice[i] = (uint)(agentObjsArr[i].priceFactor * (agentObjsArr[i].agentCount + 1));
                 }
 
-                RenderWindow.displayPoints = GameOperator.gamePoints;
+                UpdateConsole();
 
-                // Render pertinent values in a console.
-                renderObj.RenderLoop();
+                Thread.Sleep(1000);
+            }
+        }
 
+        // Method for player input thread
+        #region
+
+        static void PlayerInput()
+        {
+            while (true)
+            {
                 // Player controls
                 ConsoleKeyInfo playerInput = Console.ReadKey();
 
                 if (Char.IsNumber(playerInput.KeyChar))
                 {
                     // Increment the agent that the user inputs
-                    agentObjsArr[(int)(Char.GetNumericValue(playerInput.KeyChar)+9) % 10].agentCount++;
+                    agentObjsArr[(int)(Char.GetNumericValue(playerInput.KeyChar) + 9) % 10].agentCount++;
                 }
                 else
                 {
@@ -63,12 +74,40 @@ namespace Source
                             break;
                     }
                 }
+
+                UpdateConsole();
+
+                Thread.Sleep(100);
             }
         }
-    }
 
-    static class GameOperator
-    {
-        static public double gamePoints { get; set; }
+        #endregion
+
+        static void Main()
+        {
+            // Initialize ten agents.
+            agentObjsArr[0] = new Agent(0,1.1,10);
+            agentObjsArr[1] = new Agent(1,1.2,50);
+            agentObjsArr[2] = new Agent(2,1.4,150);
+            agentObjsArr[3] = new Agent(3,1.75,200);
+            agentObjsArr[4] = new Agent(4,1,300);
+            agentObjsArr[5] = new Agent(5,1.5,400);
+            agentObjsArr[6] = new Agent(6,2,550);
+            agentObjsArr[7] = new Agent(7,4,700);
+            agentObjsArr[8] = new Agent(8,7,1000);
+            agentObjsArr[9] = new Agent(9,10,2000);
+
+            // Initial console draw.
+            UpdateConsole();
+
+            // Instantiate threads.
+            ThreadStart gameLoop = new ThreadStart(GameLoop);
+            Thread myGameLoop = new Thread(gameLoop);
+            myGameLoop.Start();
+
+            ThreadStart inputLoop = new ThreadStart(PlayerInput);
+            Thread myInputLoop = new Thread(inputLoop);
+            myInputLoop.Start();
+        }
     }
 }
